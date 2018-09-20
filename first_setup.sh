9081858b1b01afb13f71b96e72c66558b9024107
Thibault <tmarion90@gmail.com>
2018-09-20 10:25:17 +0200

    Added tzdata configuration

--- first_setup.sh
+++ first_setup.sh
@@ -4,5 +4,6 @@ sudo apt-get update
 sudo apt-get install vim htop
 sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo dpkg-reconfigure keyboard-configuration
+sudo dpkg-reconfigure tzdata
 sudo adduser thibault systemd-journal
 bash deploy.sh
