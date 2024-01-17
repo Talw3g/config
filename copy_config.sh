e233f33b18e584a2e09cb62c64be9892d60cc4d0
Thibault <talw3g@protonmail.com>
2024-01-17 10:34:40 +0100

    Moved to LazyVim (new repo), added pycodestyle config

--- copy_config.sh
+++ copy_config.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 cd /home/thibault
-cp -ur .bashrc .gitconfig .config/nvim/init.lua .config/nvim/ftplugin config/
+cp -ur .bashrc .gitconfig .config/pycodestyle config/
 cd config/
 git add . && git commit
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
 
-mkdir -p ~/.config/nvim
 cp -u .bashrc .gitconfig ~/
-cp -u init.lua ~/.config/nvim/
-cp -ur ftplugin ~/.config/nvim/
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+cp -u pycodestyle ~/.config/
